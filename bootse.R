0df4c38cca2718682bcf386f08b8043183a0a2fc
eyildiztepe <10481428+eyildiztepe@users.noreply.github.com>
2020-12-20 16:55:11 +0300

    Update bootse.R

--- bootse.R
+++ bootse.R
@@ -5,5 +5,6 @@ bootse<-function(x,nboot=1000,est=median,SEED=TRUE,...){
 #   Compute bootstrap estimate of the standard error of the estimator est
   if(SEED)set.seed(2) # set seed of random number #generator so that results can be duplicated.
   data<-matrix(sample(x,size=length(x)*nboot, replace=TRUE), nrow=nboot)
-  bvec<-apply(data,1,est,...); bootse<-sqrt(var(bvec))
+  bvec<-apply(data,1,est,...)
+  bootse<-sqrt(var(bvec))
   bootse}
