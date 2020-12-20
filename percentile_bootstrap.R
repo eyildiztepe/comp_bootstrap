1ecbf503a78ee3b77134a431eb4ac3983d9c0bb3
eyildiztepe <10481428+eyildiztepe@users.noreply.github.com>
2020-12-20 16:59:40 +0300

    Update percentile_bootstrap.R

--- percentile_bootstrap.R
+++ percentile_bootstrap.R
@@ -1,8 +1,8 @@
 # (Bootstrap percentile CI)
 boot.perc <-function(statistic,statistic.vector, conf=.95){
-  CI <- quantile(statistic.vector,c((1-conf)/2, (1+conf)/2), type = 6);  names(CI)<-names(quantile(statistic.vector, c((1-conf)/2, (1+conf)/2)))
+  CI <- quantile(statistic.vector,c((1-conf)/2, (1+conf)/2), type = 6)
+  names(CI)<-names(quantile(statistic.vector, c((1-conf)/2, (1+conf)/2)))
   return(list("estimated"=statistic, "percentile"=CI))  }
-ci.width <- function(cis) {  # function that takes a matrix   #of conf. int. and computes the average of ci width.
-  z<-mean(cis[,2]-cis[,1]);  return(z) }
+
 
 
