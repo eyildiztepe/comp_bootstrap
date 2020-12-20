59c92534102fea6da977a72ab48671e1fc190049
eyildiztepe <10481428+eyildiztepe@users.noreply.github.com>
2020-12-20 15:04:47 +0300

    Create bootse.R

---
+++ bootse.R
@@ -0,0 +1,9 @@
+bootse<-function(x,nboot=1000,est=median,SEED=TRUE,...){
+#  Wilcox, R.R. (2017). Introduction to Robust Est. and #Hypothesis Testing. 4th Ed.  
+#   Rallfun-vxx.txt can be downloaded from 
+#  http://dornsife.usc.edu/labs/rwilcox/software/ 
+#   Compute bootstrap estimate of the standard error of the estimator est
+  if(SEED)set.seed(2) # set seed of random number #generator so that results can be duplicated.
+  data<-matrix(sample(x,size=length(x)*nboot, replace=TRUE), nrow=nboot)
+  bvec<-apply(data,1,est,...); bootse<-sqrt(var(bvec))
+  bootse}
